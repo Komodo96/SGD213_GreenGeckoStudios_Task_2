ced4fa1a90d48c60e911ab0e1c4edc9a71df0232
Komodo96 <gct005@student.usc.edu.au>
2025-05-07 14:43:57 +1000

    PlayerInput Script added
    Created a flat plane and cube object to represent the player. Added a player input script that allows the player to walk around and jump.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    
+    [SerializeField]
+    private float lookSpeed = 2f;
+    
+    [SerializeField]
+    private float jumpHeight = 2f;
+    
+    [SerializeField]
+    private Transform playerCamera;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        float mouseX = Input.GetAxis("Mouse X") * lookSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSpeed;
+
+        transform.Rotate(Vector3.up * mouseX);
+        playerCamera.Rotate(Vector3.left * mouseY);
+    }
+}
