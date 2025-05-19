7496d2b8c723e3387a176258c6faed8fd0fcc1a3
cao017 <144617479+cao017@users.noreply.github.com>
2025-05-19 18:19:36 +1000

    Grenade, and base functionality of WeaponBase, Pistol, and AssaultRifle
    Implementation of the Grenade /ui tracking of grenade count and added base functionality of WeaponBase, Pistol, and AssaultRifle

--- Assets/Scripts/AssaultRifle.cs
+++ Assets/Scripts/AssaultRifle.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssaultRifle : MonoBehaviour
+public class AssaultRifle : WeaponBase
 {
-    // Start is called before the first frame update
-    void Start()
+    // Shoot will spawn a new bullet, provided enough time has passed compared to our fireDelay.
+    public override void Shoot()
     {
-        
-    }
+        // get the current time
+        float currentTime = Time.time;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // if enough time has passed since our last shot compared to our fireDelay, spawn our bullet
+        if (currentTime - lastFiredTime > fireDelay)
+        {
+            // create our bullet
+            GameObject newBullet = Instantiate(bullet, bulletSpawnPoint.position, transform.rotation);
+
+            // update our shooting state
+            lastFiredTime = currentTime;
+        }
     }
 }
---
+++ Assets/Scripts/GrenadeThrower.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI; // For UI elements
+using TMPro;
+
+public class GrenadeThrower : MonoBehaviour
+{
+    [Header("Grenade Settings")]
+    public GameObject grenadePrefab;
+    public Transform throwPoint;
+    public float throwForce = 15f;
+    public float upwardForce = 2f;
+
+    [Header("Explosion Settings")]
+    public float grenadeLifetime = 3f; // Time before explosion
+
+    [Header("Grenade Count UI")]
+    public int maxGrenades = 3;
+    private int currentGrenades;
+    public TextMeshProUGUI grenadeCountText;
+
+    void Start()
+    {
+        currentGrenades = maxGrenades;
+        UpdateGrenadeUI();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && currentGrenades > 0)
+        {
+            ThrowGrenade();
+        }
+    }
+
+    void ThrowGrenade()
+    {
+        if (grenadePrefab == null || throwPoint == null) return;
+
+        GameObject grenade = Instantiate(grenadePrefab, throwPoint.position, throwPoint.rotation);
+
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 forceDirection = throwPoint.forward * throwForce + throwPoint.up * upwardForce;
+            rb.AddForce(forceDirection, ForceMode.VelocityChange);
+        }
+
+        currentGrenades--;
+        UpdateGrenadeUI();
+
+        StartCoroutine(ExplodeAfterDelay(grenade, grenadeLifetime));
+    }
+
+    System.Collections.IEnumerator ExplodeAfterDelay(GameObject grenade, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Debug.Log("BOOM! Grenade exploded!");
+        Destroy(grenade);
+    }
+
+    void UpdateGrenadeUI()
+    {
+        if (grenadeCountText != null)
+        {
+            grenadeCountText.text = "Grenades: " + currentGrenades;
+        }
+    }
+
+    // Optional: Call this from pickups to replenish grenades
+    public void AddGrenade(int amount)
+    {
+        currentGrenades = Mathf.Min(currentGrenades + amount, maxGrenades);
+        UpdateGrenadeUI();
+    }
+}
